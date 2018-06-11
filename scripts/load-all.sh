5880dd29fbe898e0dac95187cc5e6ab3186027d5
Shaun Smith <shaun.smith@oracle.com>
2018-06-11 13:26:26 -0400

    Updated FDK version and cleanup

--- scripts/load-all.sh
+++ scripts/load-all.sh
@@ -1,13 +1,15 @@
 #! /usr/bin/env bash
 
-docker load < fnproject-fnserver.latest.tgz 
-docker load < fnproject-flow.latest.tgz 
-docker load < fnproject-flow.ui.tgz 
-docker load < fnproject-fn-java-fdk-build.latest.tgz 
-docker load < fnproject-fn-java-fdk.latest.tgz 
-docker load < fnproject-fn-java-fdk.jdk9-latest.tgz 
-docker load < fnproject-fn-java-fdk-build.jdk9-latest.tgz
-docker load < fnproject-ui.latest.tgz
-docker load < fnproject-go.latest.tgz
-docker load < fnproject-go.dev.tgz
-docker load < node.8-alpine.tgz
+if [ -z "$1" ]; then
+  SOURCE_DIR=.
+else
+  SOURCE_DIR=${1}
+fi
+
+docker load < ${SOURCE_DIR}/fnproject-fnserver.latest.tgz 
+docker load < ${SOURCE_DIR}/fnproject-fn-java-fdk-build.latest.tgz 
+docker load < ${SOURCE_DIR}/fnproject-fn-java-fdk.latest.tgz 
+docker load < ${SOURCE_DIR}/fnproject-ui.latest.tgz
+docker load < ${SOURCE_DIR}/fnproject-go.latest.tgz
+docker load < ${SOURCE_DIR}/fnproject-go.dev.tgz
+docker load < ${SOURCE_DIR}/node.8-alpine.tgz
